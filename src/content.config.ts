603443a1fbe79d07a8db44d647010385bdfdb8f8
Rivaldo Silalahi <57586418+valdo766hi@users.noreply.github.com>
2026-03-17 16:08:57 +0700

    feat(blog): add search on the blog page  (#21)
    * feat(blog): adding search bar for blog
    
    * chore: add tags to all blogs
    
    * feat: add branded social preview image

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
 			z.object({
 				title: z.string(),
 				description: z.string(),
+				tags: z.array(z.string().min(1)).default([]),
 				writerGithub: z.string().optional(),
 				// Transform string to Date object
 				pubDate: z.coerce.date(),
